074223799372ea73624959769dbb4d4b18f67520
Brandon S. Nichols <55559537+pathspot-brandon@users.noreply.github.com>
2022-02-25 16:01:36 -0500

    Translated many of the comments

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var fs = require('fs');
-var fontCarrier = require('font-carrier')
-var path = require('path');
+var fs = require("fs");
+var fontCarrier = require("font-carrier");
+var path = require("path");
 var pinyin = require("pinyin");
-var DOMParser = require('xmldom').DOMParser
-var svgPathify = require('svg_pathify');
+var DOMParser = require("xmldom").DOMParser;
+var svgPathify = require("svg_pathify");
 
 var unicodeNum = 60000;
-var fileName = 'iconfont';
-var fontFamily = 'iconfont';
-var fontClass = 'icon-';
-
+var fileName = "iconfont";
+var fontFamily = "iconfont";
+var fontClass = "icon-";
 
 var Svg2Font = (function () {
   var cssItems = [];
@@ -29,11 +28,9 @@ var Svg2Font = (function () {
     }
   };
 
-
   return {
     reverse: function (inputPath, outputPath) {
       try {
-
         mkdirSync(outputPath, () => {
           var glyphs = fontCarrier.transfer(inputPath).allGlyph();
           const svgs = [];
@@ -42,20 +39,32 @@ var Svg2Font = (function () {
               continue;
             }
             var svg = glyphs[key].toSvg();
-            var glyphName = glyphs[key].options.name || glyphs[key].options.glyphName || key.slice(1);
-            svgs.push([
-              `<li class="dib">`,
-              `  <img class="icon svg-icon" src="data:image/svg+xml;base64,${new Buffer(svg).toString('base64')}" alt=""></img>`,
-              `  <div class="name">${glyphName}</div>`,
-              `  <div class="code-name">${key.slice(1)}</div>`,
-              `</li>`
-            ].join('\n'));
-            fs.writeFileSync(path.join(outputPath, glyphName + '.svg'), svg);
+            var glyphName =
+              glyphs[key].options.name ||
+              glyphs[key].options.glyphName ||
+              key.slice(1);
+            svgs.push(
+              [
+                `<li class="dib">`,
+                `  <img class="icon svg-icon" src="data:image/svg+xml;base64,${new Buffer(
+                  svg
+                ).toString("base64")}" alt=""></img>`,
+                `  <div class="name">${glyphName}</div>`,
+                `  <div class="code-name">${key.slice(1)}</div>`,
+                `</li>`
+              ].join("\n")
+            );
+            fs.writeFileSync(path.join(outputPath, glyphName + ".svg"), svg);
           }
 
-          let template = fs.readFileSync(path.join(__dirname, 'template', 'demo_svg.html')).toString();
-          template = template.replace('<div id="svg-list"></div>', svgs.join('\n\n'));
-          fs.writeFileSync(path.join(outputPath, 'demo_svg.html'), template);
+          let template = fs
+            .readFileSync(path.join(__dirname, "template", "demo_svg.html"))
+            .toString();
+          template = template.replace(
+            '<div id="svg-list"></div>',
+            svgs.join("\n\n")
+          );
+          fs.writeFileSync(path.join(outputPath, "demo_svg.html"), template);
         });
       } catch (e) {
         console.error(e);
@@ -64,7 +73,6 @@ var Svg2Font = (function () {
       }
     },
     generate: function (inputPath, outputPath, options) {
-
       if (options.unicodeNum) unicodeNum = options.unicodeNum;
 
       if (options.fileName) fileName = options.fileName;
@@ -74,19 +82,17 @@ var Svg2Font = (function () {
       if (options.fontClass) fontClass = options.fontClass;
 
       try {
-
         var init = function () {
           cssItems = [];
           symbols = [];
           demoUnicodeList = [];
           demoClassList = [];
           demoSymbolList = [];
-        }
+        };
 
         var travel = function (dir) {
-
           if (!fs.statSync(dir).isDirectory()) {
-            if (path.extname(dir) === '.svg') {
+            if (path.extname(dir) === ".svg") {
               readSvg(dir);
             }
           } else {
@@ -95,108 +101,146 @@ var Svg2Font = (function () {
               if (fs.statSync(pathname).isDirectory()) {
                 travel(pathname);
               } else {
-                if (path.extname(pathname) === '.svg') {
+                if (path.extname(pathname) === ".svg") {
                   readSvg(pathname);
                 }
               }
             });
           }
-        }
+        };
 
         var readSvg = function (dir) {
-
-          const unicode = '&#' + ++unicodeNum + ';';
+          const unicode = "&#" + ++unicodeNum + ";";
           const glyph = fs.readFileSync(dir).toString();
-          const svgDocNode = (new DOMParser()).parseFromString(svgPathify(glyph), 'application/xml');
-          const pathNode = svgDocNode.getElementsByTagName('path').toString();
-          const glyphName = path.basename(dir).split('.')[0];
-          const pinyinStr = pinyin(glyphName, { style: pinyin.STYLE_NORMAL }).join("");
+          const svgDocNode = new DOMParser().parseFromString(
+            svgPathify(glyph),
+            "application/xml"
+          );
+          const pathNode = svgDocNode.getElementsByTagName("path").toString();
+          const glyphName = path.basename(dir).split(".")[0];
+          const pinyinStr = pinyin(glyphName, {
+            style: pinyin.STYLE_NORMAL
+          }).join("");
           const className = fontClass + pinyinStr;
-          const content = '\\' + unicodeNum.toString(16);
+          const content = "\\" + unicodeNum.toString(16);
 
-          demoUnicodeList.push([
-            `<li class="dib">`,
-            `  <span class="icon ${fontFamily}">&#x${unicodeNum.toString(16)};</span>`,
-            `  <div class="name">${glyphName}</div>`,
-            `  <div class="code-name">&amp;#x${unicodeNum.toString(16)};</div>`,
-            `</li>`
-          ].join('\n'));
+          demoUnicodeList.push(
+            [
+              `<li class="dib">`,
+              `  <span class="icon ${fontFamily}">&#x${unicodeNum.toString(
+                16
+              )};</span>`,
+              `  <div class="name">${glyphName}</div>`,
+              `  <div class="code-name">&amp;#x${unicodeNum.toString(
+                16
+              )};</div>`,
+              `</li>`
+            ].join("\n")
+          );
 
-          demoClassList.push([
-            `<li class="dib">`,
-            `  <span class="icon ${fontFamily} ${className}"></span>`,
-            `  <div class="name">${glyphName}</div>`,
-            `  <div class="code-name">.${className}</div>`,
-            `</li>`
-          ].join('\n'));
+          demoClassList.push(
+            [
+              `<li class="dib">`,
+              `  <span class="icon ${fontFamily} ${className}"></span>`,
+              `  <div class="name">${glyphName}</div>`,
+              `  <div class="code-name">.${className}</div>`,
+              `</li>`
+            ].join("\n")
+          );
 
-          demoSymbolList.push([
-            `<li class="dib">`,
-            `  <svg class="icon svg-icon" aria-hidden="true">`,
-            `    <use xlink:href="#${className}"></use>`,
-            `  </svg>`,
-            `  <div class="name">${glyphName}</div>`,
-            `  <div class="code-name">#${className}</div>`,
-            `</li>`
-          ].join('\n'));
+          demoSymbolList.push(
+            [
+              `<li class="dib">`,
+              `  <svg class="icon svg-icon" aria-hidden="true">`,
+              `    <use xlink:href="#${className}"></use>`,
+              `  </svg>`,
+              `  <div class="name">${glyphName}</div>`,
+              `  <div class="code-name">#${className}</div>`,
+              `</li>`
+            ].join("\n")
+          );
 
-          symbols.push(`<symbol id="${className}" viewBox="0 0 1024 1024">${pathNode}</symbol>`);
+          symbols.push(
+            `<symbol id="${className}" viewBox="0 0 1024 1024">${pathNode}</symbol>`
+          );
           const items = [
             `.${className}:before {`,
             `  content: "${content}";`,
             `}`
           ];
-          cssItems.push(items.join('\n'));
+          cssItems.push(items.join("\n"));
 
           font.setGlyph(unicode, {
             svg: glyph,
             glyphName: pinyinStr
           });
-        }
+        };
 
         var outputCssJs = function () {
-          //输出css文件
-          const woff2 = fs.readFileSync(path.join(outputPath, fileName) + '.woff2').toString()
-          const base64 = Buffer.from(woff2).toString('base64')
+          //Output Css File
+          const woff2 = fs
+            .readFileSync(path.join(outputPath, fileName) + ".woff2")
+            .toString();
+          const base64 = Buffer.from(woff2).toString("base64");
           const timestamp = new Date().getTime();
-          const cssStr = [
-            `@font-face {`,
-            `  font-family: "${fontFamily}";`,
-            `  src: url('${fileName}.eot?t=${timestamp}'); /* IE9 */`,
-            `  src: url('${fileName}.eot?t=${timestamp}#iefix') format('embedded-opentype'), /* IE6-IE8 */`,
-            `  url('data:application/x-font-woff2;charset=utf-8;base64,${base64}') format('woff2'),`,
-            `  url('${fileName}.woff?t=${timestamp}') format('woff'),`,
-            `  url('${fileName}.ttf?t=${timestamp}') format('truetype'), /* chrome, firefox, opera, Safari, Android, iOS 4.2+ */`,
-            `  url('${fileName}.svg?t=${timestamp}#${fontFamily}') format('svg'); /* iOS 4.1- */`,
-            `}\n`,
-            `.${fontFamily} {`,
-            `  font-family: "${fontFamily}" !important;`,
-            `  font-size: 16px;`,
-            `  font-style: normal;`,
-            `  -webkit-font-smoothing: antialiased;`,
-            `  -moz-osx-font-smoothing: grayscale;`,
-            `}\n\n`
-          ].join('\n') + cssItems.join('\n\n');
+          const cssStr =
+            [
+              `@font-face {`,
+              `  font-family: "${fontFamily}";`,
+              `  src: url('${fileName}.eot?t=${timestamp}'); /* IE9 */`,
+              `  src: url('${fileName}.eot?t=${timestamp}#iefix') format('embedded-opentype'), /* IE6-IE8 */`,
+              `  url('data:application/x-font-woff2;charset=utf-8;base64,${base64}') format('woff2'),`,
+              `  url('${fileName}.woff?t=${timestamp}') format('woff'),`,
+              `  url('${fileName}.ttf?t=${timestamp}') format('truetype'), /* chrome, firefox, opera, Safari, Android, iOS 4.2+ */`,
+              `  url('${fileName}.svg?t=${timestamp}#${fontFamily}') format('svg'); /* iOS 4.1- */`,
+              `}\n`,
+              `.${fontFamily} {`,
+              `  font-family: "${fontFamily}" !important;`,
+              `  font-size: 16px;`,
+              `  font-style: normal;`,
+              `  -webkit-font-smoothing: antialiased;`,
+              `  -moz-osx-font-smoothing: grayscale;`,
+              `}\n\n`
+            ].join("\n") + cssItems.join("\n\n");
 
-          fs.writeFileSync(path.join(outputPath, fileName + '.css'), cssStr);
+          fs.writeFileSync(path.join(outputPath, fileName + ".css"), cssStr);
 
-          //输出js文件
-          const jsStr = `!function (a) { var t, c = '<svg>${symbols.join('')}</svg>', e = (t = document.getElementsByTagName("script"))[t.length - 1].getAttribute("data-injectcss"); if (e && !a.__iconfont__svg__cssinject__) { a.__iconfont__svg__cssinject__ = !0; try { document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>") } catch (t) { console && console.log(t) } } !function (t) { if (document.addEventListener) if (~["complete", "loaded", "interactive"].indexOf(document.readyState)) setTimeout(t, 0); else { var e = function () { document.removeEventListener("DOMContentLoaded", e, !1), t() }; document.addEventListener("DOMContentLoaded", e, !1) } else document.attachEvent && (n = t, l = a.document, o = !1, (i = function () { try { l.documentElement.doScroll("left") } catch (t) { return void setTimeout(i, 50) } c() })(), l.onreadystatechange = function () { "complete" == l.readyState && (l.onreadystatechange = null, c()) }); function c() { o || (o = !0, n()) } var n, l, o, i }(function () { var t, e; (t = document.createElement("div")).innerHTML = c, c = null, (e = t.getElementsByTagName("svg")[0]) && (e.setAttribute("aria-hidden", "true"), e.style.position = "absolute", e.style.width = 0, e.style.height = 0, e.style.overflow = "hidden", function (t, e) { e.firstChild ? function (t, e) { e.parentNode.insertBefore(t, e) }(t, e.firstChild) : e.appendChild(t) }(e, document.body)) }) }(window);`
-          fs.writeFileSync(path.join(outputPath, fileName + '.js'), jsStr);
+          //output js file
+          const jsStr = `!function (a) { var t, c = '<svg>${symbols.join(
+            ""
+          )}</svg>', e = (t = document.getElementsByTagName("script"))[t.length - 1].getAttribute("data-injectcss"); if (e && !a.__iconfont__svg__cssinject__) { a.__iconfont__svg__cssinject__ = !0; try { document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>") } catch (t) { console && console.log(t) } } !function (t) { if (document.addEventListener) if (~["complete", "loaded", "interactive"].indexOf(document.readyState)) setTimeout(t, 0); else { var e = function () { document.removeEventListener("DOMContentLoaded", e, !1), t() }; document.addEventListener("DOMContentLoaded", e, !1) } else document.attachEvent && (n = t, l = a.document, o = !1, (i = function () { try { l.documentElement.doScroll("left") } catch (t) { return void setTimeout(i, 50) } c() })(), l.onreadystatechange = function () { "complete" == l.readyState && (l.onreadystatechange = null, c()) }); function c() { o || (o = !0, n()) } var n, l, o, i }(function () { var t, e; (t = document.createElement("div")).innerHTML = c, c = null, (e = t.getElementsByTagName("svg")[0]) && (e.setAttribute("aria-hidden", "true"), e.style.position = "absolute", e.style.width = 0, e.style.height = 0, e.style.overflow = "hidden", function (t, e) { e.firstChild ? function (t, e) { e.parentNode.insertBefore(t, e) }(t, e.firstChild) : e.appendChild(t) }(e, document.body)) }) }(window);`;
+          fs.writeFileSync(path.join(outputPath, fileName + ".js"), jsStr);
         };
 
         var outputDemo = function () {
-          //输出demo-html
-          let demoHtmlStr = fs.readFileSync(path.join(__dirname, 'template', 'demo_index.html')).toString();
-          demoHtmlStr = demoHtmlStr.replace('<div id="unicode-list"></div>', demoUnicodeList.join('\n\n'));
-          demoHtmlStr = demoHtmlStr.replace('<div id="class-list"></div>', demoClassList.join('\n\n'));
-          demoHtmlStr = demoHtmlStr.replace('<div id="symbol-list"></div>', demoSymbolList.join('\n\n'));
-          demoHtmlStr = demoHtmlStr.replace('iconfont.css', `${fileName}.css`);
-          demoHtmlStr = demoHtmlStr.replace('iconfont.js', `${fileName}.js`);
-          fs.writeFileSync(path.join(outputPath, 'demo_index.html'), demoHtmlStr);
+          //Export demo-html
+          let demoHtmlStr = fs
+            .readFileSync(path.join(__dirname, "template", "demo_index.html"))
+            .toString();
+          demoHtmlStr = demoHtmlStr.replace(
+            '<div id="unicode-list"></div>',
+            demoUnicodeList.join("\n\n")
+          );
+          demoHtmlStr = demoHtmlStr.replace(
+            '<div id="class-list"></div>',
+            demoClassList.join("\n\n")
+          );
+          demoHtmlStr = demoHtmlStr.replace(
+            '<div id="symbol-list"></div>',
+            demoSymbolList.join("\n\n")
+          );
+          demoHtmlStr = demoHtmlStr.replace("iconfont.css", `${fileName}.css`);
+          demoHtmlStr = demoHtmlStr.replace("iconfont.js", `${fileName}.js`);
+          fs.writeFileSync(
+            path.join(outputPath, "demo_index.html"),
+            demoHtmlStr
+          );
 
-          //输出demo-css
-          fs.copyFileSync(path.join(__dirname, 'template', 'demo.css'), path.join(outputPath, 'demo.css'));
+          //Export demo-css
+          fs.copyFileSync(
+            path.join(__dirname, "template", "demo.css"),
+            path.join(outputPath, "demo.css")
+          );
         };
 
         const font = fontCarrier.create();
@@ -210,21 +254,20 @@ var Svg2Font = (function () {
         travel(inputPath, font);
 
         mkdirSync(outputPath, () => {
-          //输出字体文件
+          //Output font file
           font.output({
             path: path.join(outputPath, fileName)
           });
           outputCssJs(outputPath);
           outputDemo();
         });
-
       } catch (e) {
         console.error(e);
         console.error("Can't open input file (%s)", inputPath);
         throw e;
       }
     }
-  }
-})()
+  };
+})();
 
-module.exports = Svg2Font
+module.exports = Svg2Font;
--- readme.md
+++ readme.md
@@ -1,36 +1,42 @@
 # svg2font
 
-svg2font 是基于font-carrier的命令行工具，旨在由svg生成目前浏览器支持的多种字体文件。
+svg2font is a command line tool based on font-carrier, designed to generate a variety of font files supported by current browsers from an svg.
 
 ## Install
 
 ### command line
+
 ```sh
 npm install -g https://github.com/george-quzhi/svg2font.git
 ```
+
 ### node
+
 ```sh
 npm install --save https://github.com/george-quzhi/svg2font.git
 ```
+
 ## Usage
 
 ### command line
+
 ```sh
 svg2font ./test/svgs ./test/font/
 ```
+
 ```markdown
 Positional arguments:
-  svgPath               Input svg path
-  fontPath              Output font path
+svgPath Input svg path
+fontPath Output font path
 
 Optional arguments:
-  -h, --help                                Show this help message and exit.
-  -v, --version                             Show program's version number and exit.
-  -u UNICODENUM, --unicodeNum UNICODENUM    unicodeNum, 指定font的unicode起始值。default 60000。
-                                           （默认加1）&#xEA61;。 
-  -n FILENAME, --fileName FILENAME          fileName, 指定生成字体文件的名字。default iconfont。
-  -f FONTFAMILY, --fontFamily FONTFAMILY    fontFamily, 指定字体的font-family。default iconfont。
-  
+-h, --help Show this help message and exit.
+-v, --version Show program's version number and exit.
+-u UNICODENUM, --unicodeNum UNICODENUM unicodeNum, Specifies the unicode start value of the font. default 60000。
+(default plus 1)&#xEA61;。
+-n FILENAME, --fileName FILENAME fileName, Specifies the name of the generated font file。default iconfont。
+-f FONTFAMILY, --fontFamily FONTFAMILY fontFamily, font of the specified font-family。default iconfont。
+
                                               .iconfont {
                                                 font-family: "iconfont" !important;
                                                 font-size: 16px;
@@ -38,25 +44,26 @@ Optional arguments:
                                                 -webkit-font-smoothing: antialiased;
                                                 -moz-osx-font-smoothing: grayscale;
                                               }
-  
 
-  -c FONTCLASS, --fontClass FONTCLASS       fontClass, 指定图标class的前缀。default icon-。
-  
+-c FONTCLASS, --fontClass FONTCLASS fontClass, Specifies the icon class prefix. default icon-。
+
                                               .icon-test-1:before {
                                                 content: "\ea61";
                                               }
-                                              
-  -r REVERSE, --reverse REVERSE             font to svg 反向，由字体文件生成svg图片 
-                                            svg2font ./test/font/iconfont.ttf ./test/svgs/svg -r true
+
+-r REVERSE, --reverse REVERSE font to svg Reverse, generate svg image from font file
+svg2font ./test/font/iconfont.ttf ./test/svgs/svg -r true
 ```
+
 ### node
+
 ```js
 var svg2font = require("svg2font");
 svg2font.generate(svgPath, fontPath, {
-  unicodeNum: 60000, //指定font的unicode起始值。
-  fileName: 'iconfont', //指定生成字体文件的名字。
-  fontFamily: 'iconfont', //指定字体的font-family。
-  fontClass: 'icon-' //指定图标class的前缀。
+  unicodeNum: 60000, //Specifies the unicode start value of the font.
+  fileName: "iconfont", //Specifies the name of the generated font file.
+  fontFamily: "iconfont", //Specifies the font-family of the font.
+  fontClass: "icon-" //Specifies the icon class prefix.
 });
 ```
 
